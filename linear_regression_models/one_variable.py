5970a040bd157bb1e4410219b6836683e55ad388
Shivani Karikar <karikarshivani@gmail.com>
2023-06-21 22:33:24 -0400

    Staged functions for gradient descent
    Signed-off-by: Shivani Karikar <karikarshivani@gmail.com>

--- linear_regression_models/one_variable.py
+++ linear_regression_models/one_variable.py
@@ -13,14 +13,11 @@ x_train = np.array(x_feature_values)
 y_target_values = [300.0, 350.0, 380.0, 460.0, 485.0, 500.0] # price in 1000s of dollars
 y_train = np.array(y_target_values)
 
-def compute_model_output(x, w, b): # Calculating f(x) = wx + b for training data
-    m = len(x_feature_values)
-    f_wb = np.zeros(m) # TODO: Find out whether preallocation is faster or appending
+w = 200
+b = 100
 
-    for i in range(m):
-        f_wb[i] = w * x[i] + b
-
-    return f_wb
+def compute_gradient():
+    pass
 
 def compute_cost(x, y, w, b): # Calculation cost function J to find ideal value for w
     m = len(x)
@@ -33,8 +30,17 @@ def compute_cost(x, y, w, b): # Calculation cost function J to find ideal value 
 
     return total_cost
 
-w = 200
-b = 100
+def gradient_descent():
+    pass
+
+def compute_model_output(x, w, b): # Calculating f(x) = wx + b for training data
+    m = len(x_feature_values)
+    f_wb = np.zeros(m) # TODO: Find out whether preallocation is faster or appending
+
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+
+    return f_wb
 
 temp_f_wb = compute_model_output(x_train, w, b)
 plt.plot(x_train, temp_f_wb, c='purple', label='Prediction Graph') # TODO: Find diff between plot and scatter functions
